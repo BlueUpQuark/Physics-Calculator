7e814a8432353924f62a283bc8bbda98fb86344e
BlueUpQuark <justin.carlton96@gmail.com>
2025-04-19 02:25:59 -0400

    Test
    //

--- SecondLawChoices.cpp
+++ SecondLawChoices.cpp
@@ -11,4 +11,10 @@ void SecondLawChoices::netForce() {
 	secondlawchoices.setM(getUserInput());
 
 	std::cout << "Okay, the mass will be " << secondlawchoices.getM() << " kg's" << std::endl;
+	std::cout << "Now, what will the acceleration be? " << std::endl;
+
+	secondlawchoices.setA(getUserInput());
+
+	std::cout << "Okay, the acceleration will be " << secondlawchoices.getA() << " meters per second squared." << std::endl;
 }
+
